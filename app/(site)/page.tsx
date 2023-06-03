d97c05d8584a0bd262108f06b5a3962dc39e27b8
Pedro Prebeck <pedroazevedo252@gmail.com>
2023-06-03 02:18:45 -0300

    refactor(app): update site layout and add header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,18 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-green-500">
-      Hello (Site)!
+    <div
+      className="
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto
+      rounded-lg
+      bg-neutral-900
+    "
+    >
+      <Header>Header</Header>
     </div>
-  )
+  );
 }
